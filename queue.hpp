b19edd0874df2e6e1cae554f7a4fceaccb9ea173
nate0m <nathanmilton82@gmail.com>
2023-04-25 11:43:31 -0700

    Revert "fix"
    This reverts commit 7742e395f77e0d6c885eb70475ec80091a743848.

--- queue.hpp
+++ queue.hpp
@@ -19,23 +19,17 @@ template<typename T>
 class Queue {
 
     public:
-        // PRE - 
-        // POST - Queue object create with capacity of 10 if not otherwise specified
         explicit Queue(int capacity = 10) : head_{-1}, tail_{-1}, size_{0}, capacity_{capacity}, data_{make_unique<T[]>(capacity)} {}
 
-        // PRE - other = Queue object
-        // POST - other is copied to new object
         Queue(const Queue<T>& other) : head_{other.head_}, tail_{other.tail_}, size_{other.size_}, capacity_{other.capacity_}, data_{make_unique<T[]>(other.capacity_)} {
 
             int i = head_;
             do { 
                 data_[i] = other.data_[i];
-                i = (i + 1) % capacity_;// move i to next element in circular buffer
-            } while (i != (tail_ + 1) % capacity_);//while i != element after last element
+                i = (i + 1) % capacity_;
+            } while (i != (tail_ + 1) % capacity_);
         }
 
-        // PRE - other = Queue object
-        // POST - other is copied to new object
         Queue<T>& operator=(const Queue<T>& other) {
             head_ = other.head_;
             tail_ = other.tail_;
@@ -52,17 +46,10 @@ class Queue {
             return *this;
         }
 
-        // PRE - other = r value Queue object
-        // POST - other is moved to new Queue object; other is nullptr
-        Queue(Queue<T>&& other) noexcept = default;
+        Queue(Queue<T>&& other) = default;
 
+        Queue<T>& operator=(Queue<T>&& other) = default;
 
-        // PRE - other = r value Queue object
-        // POST - other is moved to new Queue object; other is nullptr
-        Queue<T>& operator=(Queue<T>&& other) noexcept = default;
-
-        // PRE - element = value of type T
-        // POST - value is added end of queue 
         void enqueue(const T& element) {
             if(isFull())
                 throw runtime_error("Queue is Full");
@@ -77,8 +64,6 @@ class Queue {
             //cerr << "tail = " << tail_ << endl;
         }
 
-        // PRE - queue has at least one value in it
-        // POST - element at begining of list is removed and returned 
         T dequeue() {
             if(isEmpty()) {
                 throw runtime_error("Queue is Empty");
@@ -95,53 +80,42 @@ class Queue {
             //cerr << "tail = " << tail_ << endl;
             return elem;
         }
-
-        // PRE - queue has at least one object
-        // POST - returns element at beginning of queue 
         T& front() {
             if(isEmpty())
                 throw runtime_error("Queue is empty");
             return data_[head_];
         }
-
-        // PRE - queue object
-        // POST - returns bool of if queue is empty
         bool isEmpty() const {
             return head_ == -1 && tail_ == -1;
         }
         
-        // PRE - queue object
-        // POST - returns bool of if queue is full
         bool isFull() const {
             return (tail_ + 1) % capacity_ == head_;
         }
 
-        // PRE - queue object
-        // POST - returns int of size of queue
         int size() const {
             return size_;
         }
 
-        // PRE - queue object has at least one value
-        // POST - prints queue to console
-        void print() const {
-            if (isEmpty()) {
-                cout << "Queue is empty" << endl;
-                return;
-            }
-            int i = head_;
-            do {
-                cout << data_[i] << " ";
-                i = (i + 1) % capacity_;
-            } while (i != (tail_ + 1) % capacity_);
-            cout << endl;
+    void print() const {
+        if (isEmpty()) {
+            cout << "Queue is empty" << endl;
+            return;
         }
+        int i = head_;
+        do {
+            cout << data_[i] << " ";
+            i = (i + 1) % capacity_;
+        } while (i != (tail_ + 1) % capacity_);
+        cout << endl;
+    }
+
 
     private:
-        int head_; // first
-        int tail_; // last
-        int size_; // count
-        int capacity_; // max size
-        unique_ptr<T[]> data_;// array of T elements 
+        int head_;
+        int tail_;
+        int size_;
+        int capacity_;
+        unique_ptr<T[]> data_; 
 };
 
